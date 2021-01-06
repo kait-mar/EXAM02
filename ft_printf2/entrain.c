484f354683c074776fcbc08619afbdf9b3015633
Khaoula Ait maryam <kait-mar@e1r11p6.1337.ma>
2021-01-06 18:15:21 +0100

    final printf

--- ft_printf2/entrain.c
+++ ft_printf2/entrain.c
@@ -17,7 +17,7 @@ char	num_char(int n);
 int		is_format(char c);
 int		len_hexa(unsigned int n);
 int		ft_atoi(char *s);
-void	ft_putnbr(int n);
+void	ft_putnbr(long n);
 int		ft_printf(const char *fromat, ...);
 void	const_struct(t_list *str, const char *format, va_list list);
 int		ft_isdigit(char c);
@@ -126,7 +126,7 @@ int		is_format(char c)
 	return (c == 'd' || c == 's' || c == 'x');
 }
 
-void	ft_putnbr(int n)
+void	ft_putnbr(long n)
 {
 	if (n < 0)
 	{
@@ -185,6 +185,8 @@ void	const_struct(t_list *str, const char *format, va_list list)
 			s = "(null)";
 		str->s = s;
 	}
+	else if (format[i] == 'x')
+		str->x = va_arg(list, unsigned int);
 }
 
 int		ft_isdigit(char c)
@@ -194,7 +196,7 @@ int		ft_isdigit(char c)
 
 void	print(t_list *str, const char **format)
 {
-	if (ft_isdigit(**format))
+	if (ft_isdigit(**format) || find((char *)*format, '.'))
 		print_prec(str, format);
 	else if (is_format(**format))
 		print_format(str, *format);
@@ -223,9 +225,11 @@ void	print_prec(t_list *str, const char **format)
 	int		i;
 	int		j;
 	int		k;
+	int		yes;
 
 	i = 0;
-	while (ft_isdigit(*format[i]))
+	yes = 0;
+	while ((*format)[i] && ft_isdigit((*format)[i]))
 		i++;
 	width = malloc(i + 1);
 	i = 0;
@@ -238,9 +242,12 @@ void	print_prec(t_list *str, const char **format)
 	i = ft_atoi(width);
 	free(width);
 	if (**format == '.')
+	{
+		yes = 1;
 		(*format)++;
+	}
 	j = 0;
-	while (ft_isdigit(*format[j]))
+	while (ft_isdigit((*format)[j]))
 		j++;
 	width = malloc(j + 1);
 	j = 0;
@@ -252,20 +259,30 @@ void	print_prec(t_list *str, const char **format)
 	width[j] = '\0';
 	j = ft_atoi(width);
 	free(width);
-	if (**format == 's')
-		k = i - ft_strlen(str->s);
+	if (yes == 1 && j == 0 && ((**format == 'd' && str->d == 0) || (**format == 'x' && str->x == 0)))
+	{
+		while (--i >= 0)
+			ft_putchar(' ');
+	}
 	else
-		k = i - j - len(str, (char *)*format);
-	while (--k >= 0)
-		ft_putchar(' ');
-	if (**format == 's')
-		j = 0;
-	if (**format == 'd' && str->d < 0)
-		ft_putchar('-');
-	k = j - ft_len(str, *format);
-	while (--k >= 0)
-		ft_putchar('0');
-	print_format2(str, *format);
+	{
+		if (**format == 's')
+			k = i - ft_strlen(str->s);
+		else if (j > ft_len(str, *format))
+			k = i - j - len(str, (char *)*format);
+		else
+			k = i - ft_len(str, (char *)*format);
+		while (--k >= 0)
+			ft_putchar(' ');
+		if (**format == 's')
+			j = 0;
+		if (**format == 'd' && str->d < 0)
+			ft_putchar('-');
+		k = j - ft_len(str, *format);
+		while (--k >= 0)
+			ft_putchar('0');
+		print_format2(str, *format);
+	}
 }
 
 int		len(t_list *str, const char *format)
@@ -277,15 +294,29 @@ int		len(t_list *str, const char *format)
 	return (0);
 }
 
+int		find(char *s, char c)
+{
+	while (*s && is_format(*s) == 0)
+	{
+		if (*s == c)
+			return (1);
+		s++;
+	}
+	return (0);
+}
+
 void	print_format2(t_list *str, const char *format)
 {
 	char	*s;
+	long	nb;
 
 	if (*format == 'd')
 	{
 		if (str->d < 0)
-			str->d *= -1;
-		ft_putnbr(str->d);
+			nb = (str->d) * (-1);
+		if (nb < 0)
+			nb = -nb;
+		ft_putnbr(nb);
 	}
 	else if (*format == 's')
 		ft_putstr(str->s);
@@ -323,6 +354,7 @@ char	*dec_to_hexa(unsigned int n)
 	converted[n] = '\0';
 	return (converted);
 }
+
 int main()
 {
   int i;
@@ -330,8 +362,8 @@ int main()
   char *s = NULL;
    // printf("hello world %s, yes %5.5x %s\n", s, -5485846, s);
     //ft_printf("hello world, yes %15.9x %d\n", 51684, 3);
-    printf("hi%4.9s", "hi");
-    ft_printf("hi%4.9s", "hi");
+    printf("|%5.0d|\n", -2147483648);
+    ft_printf("|%5.0d|\n", 	-2147483648);
   // printf("|%5.3d|\n", 6);
   //precision avec s et x
   //while (1)
